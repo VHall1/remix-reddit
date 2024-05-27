b5e33137a24a578ef4d73027ea1e17b092cceb89
VHall1 <victorhallpsn@hotmail.com>
2024-05-28 00:34:47 +0100

    stub new post route

--- app/routes/posts.new.tsx
+++ app/routes/posts.new.tsx
@@ -1,17 +1,41 @@
 import { serverOnly$ } from "vite-env-only/macros";
 import { Shell } from "~/components/shell";
+import { Button } from "~/components/ui/button";
+import { Card, CardContent, CardFooter } from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Textarea } from "~/components/ui/textarea";
 import { requireAuth } from "~/middleware/require-auth";
-
-export const middleware = serverOnly$([requireAuth]);
 
 export default function NewPost() {
   return (
-    <Shell>
-      <p>new post</p>
+    <Shell className="py-6 container max-w-screen-lg">
+      <Card>
+        <CardContent className="py-6 grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="title">Title</Label>
+            <Input id="title" name="title" placeholder="Title" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="content">Content</Label>
+            <Textarea
+              id="content"
+              name="content"
+              placeholder="Content (optional)"
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Post</Button>
+        </CardFooter>
+      </Card>
     </Shell>
   );
 }
 
+export const middleware = serverOnly$([requireAuth]);
+
+// need this loader here otherwise the middleware won't be called
 export function loader() {
   return null;
 }
