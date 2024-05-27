e0344d4f7ebc5af53739bcc77c3b5b81e21f50d2
VHall1 <victorhallpsn@hotmail.com>
2024-05-27 16:55:27 +0100

    stub front page example

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,60 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { Link, useLoaderData } from "@remix-run/react";
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
+import { db } from "~/services/database.server";
+import { timeSince } from "~/utils/time";
 
 export default function Index() {
-  return <p>Hello</p>;
+  const { posts } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="container max-w-screen-lg">
+      <div
+        className="h-32 w-full rounded-lg bg-center bg-cover mt-1 mb-4 border"
+        style={{
+          backgroundImage:
+            "url('https://styles.redditmedia.com/t5_2savw/styles/bannerBackgroundImage_3e4vaale0qg41.jpg?format=pjpg&s=11eed5d0d926f002858a845d40cddc6609c8e2fc')",
+        }}
+      />
+      <div className="grid gap-4">
+        {posts.map((post) => (
+          <Card key={`post-${post.id}`}>
+            <CardHeader className="text-sm pb-0 mb-1">
+              <span>
+                <Link to="#" className="font-medium">
+                  {post.author.username}
+                </Link>{" "}
+                &bull;{" "}
+                <span
+                  className="text-muted-foreground"
+                  suppressHydrationWarning
+                >
+                  {timeSince(new Date(post.createdAt))}{" "}
+                </span>
+              </span>
+            </CardHeader>
+            <CardContent>
+              <div className="text-lg font-medium mb-2">{post.title}</div>
+              <p className="leading-7">{post.content}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function loader() {
+  const posts = await db.post.findMany({
+    where: { published: true },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      author: { select: { username: true } },
+    },
+    orderBy: { createdAt: "desc" },
+    take: 10,
+  });
+  return { posts };
 }
