3bfbb388a8537eed58fb8baf2cd45b038ca6b7f7
VHall1 <victorhallpsn@hotmail.com>
2024-05-27 20:18:02 +0100

    use auth logic to render shell

--- app/components/shell.tsx
+++ app/components/shell.tsx
@@ -1,4 +1,6 @@
+import { Link, useFetcher, useRouteLoaderData } from "@remix-run/react";
 import { type ReactNode } from "react";
+import { type loader as rootLoader } from "~/root";
 import { cn } from "~/utils/cn";
 import { Button } from "./ui/button";
 
@@ -9,11 +11,25 @@ export function Shell({
   children: ReactNode;
   className?: string;
 }) {
+  const logoutFetcher = useFetcher<unknown>();
+  const rootData = useRouteLoaderData<typeof rootLoader>("root");
+  const userId = rootData?.userId;
+
   return (
     <div className="flex flex-col min-h-screen">
       <header className="px-2 flex h-14 items-center border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="ml-auto">
-          <Button>Log in</Button>
+          {userId ? (
+            <logoutFetcher.Form method="post" action="/action/logout">
+              <Button variant="secondary" type="submit">
+                Log out
+              </Button>
+            </logoutFetcher.Form>
+          ) : (
+            <Button asChild>
+              <Link to="/login">Log in</Link>
+            </Button>
+          )}
         </div>
       </header>
       <div className="flex flex-1">
